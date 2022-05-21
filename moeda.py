2c944b4ea93db9c3781042860b14fbadebff3df8
Luis Carlos <100967943+LuisCarlosss@users.noreply.github.com>
2022-05-20 23:29:54 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -1,22 +1,39 @@
 import requests
 import json
 
-#Site para fazer requisição
+#Site usado fazer requisição
 url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
 
-def moeda():
+#Função de cotação de moeda
+def moeda(moeda):
     #Fazer requisição
     r = requests.get(url)
     j = r.json()
 
+    #Cotação da moedas
     #USDBRL 'EURBRL' 'BTCBRL'
     dolar = j['USDBRL']['bid']
     euro = j['EURBRL']['bid']
     bitcoin = j['BTCBRL']['bid']
     
-    return'''
-    Valor do Dolar é R$ {}
-    Valor do Euro é R$ {} 
-    Valor do Bitcoin é R$ {} '''.format(dolar,euro,bitcoin)
+    #Condições 
+    if moeda == 1:
+        return f'Valor do Dolar é R$ {dolar}'
+    elif moeda == 2:
+        return f'Valor do Euro é R$ {euro}'
+    elif moeda == 3:
+        return f'Valor do Bitcoin é R$ {bitcoin}'
+    else:
+        print('Moeda Não encontrada!')
+        print('Tente novamente!')
+        
+#loop infinito
+while True: 
+    #Opções 
+    print(moeda(int(input('''
+    [1] Dolar
+    [2] Euro
+    [3] Bitcoin
 
-print(moeda())
+    Digite:'''))))
+    continue
