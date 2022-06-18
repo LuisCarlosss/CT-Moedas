0635bcd72e259825dc353d7323ee448b0c2141ad
Luis Carlos <100967943+LuisCarlosss@users.noreply.github.com>
2022-06-17 22:18:59 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -1,39 +1,39 @@
+from webbrowser import get
 import requests
 import json
 
-#Site usado fazer requisição
-url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+
 
 #Função de cotação de moeda
-def moeda(moeda):
+def get_coin(coin):
+    #Site usado fazer requisição
+    url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+
     #Fazer requisição
     r = requests.get(url)
     j = r.json()
 
     #Cotação da moedas
     #USDBRL 'EURBRL' 'BTCBRL'
-    dolar = j['USDBRL']['bid']
-    euro = j['EURBRL']['bid']
+    dolar = j['USDBRL']['bid'][0:4]
+    euro = j['EURBRL']['bid'][0:4]
     bitcoin = j['BTCBRL']['bid']
     
-    #Condições 
-    if moeda == 1:
-        return f'Valor do Dolar é R$ {dolar}'
-    elif moeda == 2:
-        return f'Valor do Euro é R$ {euro}'
-    elif moeda == 3:
-        return f'Valor do Bitcoin é R$ {bitcoin}'
+    #Estrutura de codição
+    if coin == 1:
+        print(f'Valor do dolar é {dolar}')
+    elif coin == 2:
+        print(f'Valor do Euro é {euro}')
+    elif coin == 3:
+        print(f'Valor do bitcoin é {bitcoin}')
     else:
-        print('Moeda Não encontrada!')
-        print('Tente novamente!')
-        
-#loop infinito
-while True: 
-    #Opções 
-    print(moeda(int(input('''
+        print('Moeda não encontrada!')
+
+while True:     
+    #Mostra opção
+    print('''
     [1] Dolar
     [2] Euro
-    [3] Bitcoin
-
-    Digite:'''))))
-    continue
+    [3] Bitcoin\n''')
+    
+    get_coin(int(input('Digite:')))
